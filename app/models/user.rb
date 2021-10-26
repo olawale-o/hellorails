da8ee42378e63e723697bc99ab57334570a281ec
olawale-o <omoogunolawale@gmail.com>
2021-10-26 13:23:54 +0100

    Create relationship between user  and posts and comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
+# class User < ApplicationRecord
+#   has_many :created_posts, class_name: 'Post', foreign_key: :author_id
+#   has_many :comments, foreign_key: :author_id, class_name: 'Comment'
+#   has_many :posts, through: :comments
+#   has_many :likes, foreign_key: :author_id, class_name: 'Like'
+#   has_many :posts, through: :likes
+# end
+
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :posts, through: :comments
+  # These gets only all the posts created by a specific user
+  has_many :created_posts, class_name: 'Post'
+  # these gets all the comments made by a specific user
+  has_many :comments
+  # these gets all the posts created by a specifc user that has been commented on
+  has_many :commented_posts, through: :comments, source: :user, class_name: 'Comment'
+  # these gets all the likes of a specific user
+  has_many :likes, class_name: 'Like'
+  # hese gets all the posts liked by a specifc user
+  has_many :posts, through: :likes
 end
