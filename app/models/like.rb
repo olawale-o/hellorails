2bf590ffc5ec18a4bb915febb8a4acda6930a92a
olawale-o <omoogunolawale@gmail.com>
2021-10-29 14:43:33 +0100

    Define update_post_likes_counter class and instance methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,15 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # class method to update likes counter for every time a post is liked on
+  def self.update_post_likes_counter(like_id)
+    post = Like.find(like_id).post
+    post.update_column('likes_counter', post.likes_counter + 1)
+  end
+
+  # instance method to update likes counter for every time a post is liked on
+  def update_post_likes_counter
+    post.update_column('likes_counter', post.likes_counter + 1)
+  end
 end
