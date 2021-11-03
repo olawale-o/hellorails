53625ddad7ed87ca0441d9e029aba1616e58b098
olawale-o <omoogunolawale@gmail.com>
2021-11-03 13:26:51 +0100

    Remove null constraints and set defaults

---
+++ db/migrate/20211103121717_remove_null_constraint_in_users.rb
@@ -0,0 +1,5 @@
+class RemoveNullConstraintInUsers < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :users, :bio, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_054018) do
+ActiveRecord::Schema.define(version: 2021_11_03_121857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 2021_11_03_054018) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "title", null: false
-    t.text "text", null: false
+    t.string "title"
+    t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "comments_counter", default: 0, null: false
@@ -48,10 +48,10 @@ ActiveRecord::Schema.define(version: 2021_11_03_054018) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "photo", default: "https://i.pravatar.cc/150?img=2", null: false
-    t.text "bio", null: false
+    t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "posts_counter", null: false
+    t.integer "posts_counter", default: 0, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
