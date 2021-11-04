0c708667bcb2294d3f04cb752ca03e27483daf79
olawale-o <omoogunolawale@gmail.com>
2021-11-04 13:38:16 +0100

    Add destroy comments and likes if post deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,11 +15,11 @@ class Post < ApplicationRecord
   # these gets user that created a specific post
   belongs_to :user, counter_cache: :posts_counter
   # these gets all the comments made on a specific post
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # these gets all the users that made comments on a specific post
   has_many :user_comments, through: :comments, source: :post, class_name: 'Comment'
   # these gets all the likes for a specific post
-  has_many :likes
+  has_many :likes, dependent: :destroy
   # these gets all the users that liked a specific post
   has_many :users, through: :likes
   # enforce presence of user on specific post creation
